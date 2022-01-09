eb7c0da66c97585c051671c744983588ae8292ef
karlglans <karl_glans@hotmail.com>
2022-01-09 02:11:30 +0100

    removing redundant textspace

--- webbshop2/Data/ApplicationDbContext.cs
+++ webbshop2/Data/ApplicationDbContext.cs
@@ -35,7 +35,8 @@ namespace webbshop2.Data
                     Name = "User",
                     NormalizedName = "USER",
                     ConcurrencyStamp = "82261118-f4a2-410b-a505-0abc05c50f0b"
-                }, new IdentityRole
+                },
+                new IdentityRole
                 {
                     Id = UserRolesId.Admin,
                     Name = "Admin",
@@ -43,64 +44,48 @@ namespace webbshop2.Data
                     ConcurrencyStamp = "cdcaa373-39c4-4c9d-ada2-af67cead244f"
                 });
 
+            modelBuilder.Entity<ApplicationUser>().HasData(
+                new ApplicationUser
+                {
+                    Id = adminId,
+                    UserName = "admin",
+                    NormalizedUserName = "ADMIN",
+                    Email = "admin@mail.com",
+                    NormalizedEmail = "ADMIN@MAIL.COM",
+                    EmailConfirmed = false,
+                    PasswordHash = "AQAAAAEAACcQAAAAENOinM9VCukuhFocb5q4uKg6YyqXUfPmTjNYp9s39yO/i3UbieyWLon9JXTeeZvfSw==",
+                    SecurityStamp = "JLWGJISJQWEYTH3BZGOBFQTWVPHLCGMJ",
+                    ConcurrencyStamp = "697e2c0e-ec25-43e5-9bd1-ceecf0751f0b"
+                },
+                new ApplicationUser
+                {
+                    Id = user10Id,
+                    UserName = "user10",
+                    NormalizedUserName = "USER10",
+                    Email = "user10@mail.com",
+                    NormalizedEmail = "USER10@MAIL.COM",
+                    EmailConfirmed = false,
+                    PasswordHash = "AQAAAAEAACcQAAAAEGrOtfDoRSMoDzZIJQezM2L6RZT6p29njfdR39As1sVaayeyQdTmO/B7gpFyU7oWIw==",
+                    SecurityStamp = "KN4FJD2GJLMBVIJVAI3DQ2V7KNF5MMPC",
+                    ConcurrencyStamp = "7d1278d2-5d20-4751-ba32-f7583a77378b"
+                },
+                new ApplicationUser
+                {
+                    Id = user11Id,
+                    UserName = "user11",
+                    NormalizedUserName = "USER11",
+                    Email = "user11@mail.com",
+                    NormalizedEmail = "USER11@MAIL.COM",
+                    EmailConfirmed = false,
+                    PasswordHash = "AQAAAAEAACcQAAAAEE0TKhUTEhl/cl+l4FLqd4j+pCxcXTaFLzA3NcEaqf8ehF32TrTPivjd8btLJOTFAg==",
+                    SecurityStamp = "Y6QUHHD5OKKA5OWYUV3TKEMQBAYWUMJX",
+                    ConcurrencyStamp = "d8775de6-d2ce-4954-b688-c044d5fb7847"
+                });
 
-            modelBuilder.Entity<ApplicationUser>().HasData(new ApplicationUser
-            {
-                Id = adminId,
-                UserName = "admin",
-                NormalizedUserName = "ADMIN",
-                Email = "admin@mail.com",
-                NormalizedEmail = "ADMIN@MAIL.COM",
-                EmailConfirmed = false,
-                PasswordHash = "AQAAAAEAACcQAAAAENOinM9VCukuhFocb5q4uKg6YyqXUfPmTjNYp9s39yO/i3UbieyWLon9JXTeeZvfSw==",
-                SecurityStamp = "JLWGJISJQWEYTH3BZGOBFQTWVPHLCGMJ",
-                ConcurrencyStamp = "697e2c0e-ec25-43e5-9bd1-ceecf0751f0b"
-            });
-            modelBuilder.Entity<ApplicationUser>().HasData(new ApplicationUser
-            {
-                Id = user10Id,
-                UserName = "user10",
-                NormalizedUserName = "USER10",
-                Email = "user10@mail.com",
-                NormalizedEmail = "USER10@MAIL.COM",
-                EmailConfirmed = false,
-                PasswordHash = "AQAAAAEAACcQAAAAEGrOtfDoRSMoDzZIJQezM2L6RZT6p29njfdR39As1sVaayeyQdTmO/B7gpFyU7oWIw==",
-                SecurityStamp = "KN4FJD2GJLMBVIJVAI3DQ2V7KNF5MMPC",
-                ConcurrencyStamp = "7d1278d2-5d20-4751-ba32-f7583a77378b"
-            });
-            modelBuilder.Entity<ApplicationUser>().HasData(new ApplicationUser
-            {
-                Id = user11Id,
-                UserName = "user11",
-                NormalizedUserName = "USER11",
-                Email = "user11@mail.com",
-                NormalizedEmail = "USER11@MAIL.COM",
-                EmailConfirmed = false,
-                PasswordHash = "AQAAAAEAACcQAAAAEE0TKhUTEhl/cl+l4FLqd4j+pCxcXTaFLzA3NcEaqf8ehF32TrTPivjd8btLJOTFAg==",
-                SecurityStamp = "Y6QUHHD5OKKA5OWYUV3TKEMQBAYWUMJX",
-                ConcurrencyStamp = "d8775de6-d2ce-4954-b688-c044d5fb7847"
-            });
-
             modelBuilder.Entity<IdentityUserRole<string>>().HasData(
-                new IdentityUserRole<string>
-                {
-                    RoleId = UserRolesId.Admin,
-                    UserId = adminId
-                }
-            );
-            modelBuilder.Entity<IdentityUserRole<string>>().HasData(
-                new IdentityUserRole<string>
-                {
-                    RoleId = UserRolesId.User,
-                    UserId = user10Id
-                }
-            );
-            modelBuilder.Entity<IdentityUserRole<string>>().HasData(
-                new IdentityUserRole<string>
-                {
-                    RoleId = UserRolesId.User,
-                    UserId = user11Id
-                }
+                new IdentityUserRole<string> { RoleId = UserRolesId.Admin, UserId = adminId },
+                new IdentityUserRole<string> { RoleId = UserRolesId.User, UserId = user10Id },
+                new IdentityUserRole<string> { RoleId = UserRolesId.User, UserId = user11Id }
             );
         }
     }
