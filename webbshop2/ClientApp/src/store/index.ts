0ac2880f4211659c84559563a362c9713e0acdbf
Karl Glans <karlglans@users.noreply.github.com>
2022-01-10 01:14:22 +0100

    removing pages from boilerplate (#8)

--- webbshop2/ClientApp/src/store/index.ts
+++ webbshop2/ClientApp/src/store/index.ts
@@ -1,13 +1,9 @@
-import * as WeatherForecasts from './WeatherForecasts';
-import * as Counter from './Counter';
 import * as Products from './Products';
 import * as Auth from './Auth';
 import * as ShoppingCart from './ShoppingCart';
 
 // The top-level state object
 export interface ApplicationState {
-    counter: Counter.CounterState | undefined;
-    weatherForecasts: WeatherForecasts.WeatherForecastsState | undefined;
     products: Products.ProductsState | undefined;
     auth: Auth.AuthState | undefined;
     cart: ShoppingCart.ShoppingCartState | undefined;
@@ -17,8 +13,6 @@ export interface ApplicationState {
 // the reducer with the matching name. It's important that the names match exactly, and that the reducer
 // acts on the corresponding ApplicationState property type.
 export const reducers = {
-    counter: Counter.reducer,
-    weatherForecasts: WeatherForecasts.reducer,
     products: Products.reducer,
     auth: Auth.reducer,
     cart: ShoppingCart.reducer,
