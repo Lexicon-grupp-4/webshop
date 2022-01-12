0495961310ca54de02d65a265755b6132106ab2b
Karl Glans <karlglans@users.noreply.github.com>
2022-01-12 12:10:17 +0100

    adding react hook forms and fontawesome (#18)

--- webbshop2/ClientApp/src/components/NavShoppingCart.tsx
+++ webbshop2/ClientApp/src/components/NavShoppingCart.tsx
@@ -3,13 +3,17 @@ import { NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectOrderItems } from '../store/ShoppingCart';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 export default function NavShoppingCart() {
     const items = useSelector(selectOrderItems);
     const count = items.length; // TODO count item.quantity
     return (
         <NavItem>
-            <NavLink tag={Link} className="text-dark" to="/cart">Varukorg ({count})</NavLink>
+            <NavLink tag={Link} className="text-dark" to="/cart">
+                <FontAwesomeIcon icon={faShoppingCart}/>({count})
+            </NavLink>
         </NavItem>
     )
 }
