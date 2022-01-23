01bfae02d271f4d51c41d05db3b388f2bc6d2ed7
devalalbin <vala@student.chalmers.se>
2022-01-23 22:36:27 +0100

    React hook working for login + button disabled

--- webbshop2/ClientApp/src/components/LoginPage.tsx
+++ webbshop2/ClientApp/src/components/LoginPage.tsx
@@ -1,30 +1,52 @@
+import { useForm, SubmitHandler, } from "react-hook-form";
+import './LoginPage.css';
+import { Link } from "react-router-dom";
 import React, { SyntheticEvent, useState } from 'react';
 import { Button, Form, FormGroup, Input, Label, Alert } from 'reactstrap';
 import { useDispatch } from 'react-redux';
 import {
-    CREDENTIALS_LOGIN_SUCCESS, 
+    CREDENTIALS_LOGIN_SUCCESS,
     CREDENTIALS_LOGIN_FAILURE,
     ATTEMPT_LOGIN,
     PasswordLoginResponse
 } from '../store/Auth';
 import './LoginPage.css';
-import { Link } from "react-router-dom";
 import { setToken, removeToken } from '../tokenService';
+
+
 
 export default function LoginPage() {
     // TODO Add form validation and disable submit button accordingly
-    const [userName, setUserName] = useState('');
-    const [password, setPassword] = useState('');
+
+    const {
+        register,
+        handleSubmit, 
+        watch,
+        formState: { errors, isSubmitting, isDirty, isValid },
+    } = useForm<Inputs>({ mode: "onChange" });
+    
+    type Inputs = {
+        userName: string;
+        password: string;
+    };
+
+
     const [loginFailure, setLoginFailure] = useState(false);
     const dispatch = useDispatch();
-    function handleSubmit(event: SyntheticEvent) {
-        event.preventDefault();
-        setUserName('');
-        setPassword('');
+    var [userName, setUserName] = useState('');
+    var [password, setPassword] = useState('');
+
+
+    const onSubmit: SubmitHandler<Inputs> = (input) => {
+        // TODO fix Login stuff on submit 
+        userName = input.userName;
+        password = input.password;
+
+        // your form submit function which will invoke after successful validation
         fetch(`api/auth/login`, {
             method: 'POST',
-            headers: {'Content-Type': 'application/json'}, 
-            body: JSON.stringify({ name: userName, password })
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ name: userName, password  })
         })
             .then(response => {
                 if (!response.ok) {
@@ -37,46 +59,39 @@ export default function LoginPage() {
                 dispatch({ type: CREDENTIALS_LOGIN_SUCCESS, jwt: data.jwt, user: data.user });
                 setToken(data.jwt);
             })
-            .catch(()=> {
+            .catch(() => {
                 dispatch({ type: CREDENTIALS_LOGIN_FAILURE });
                 console.error('removing bad auth-token');
                 removeToken();
             });
-        dispatch({ type: ATTEMPT_LOGIN });
-    }
+        dispatch({ type: ATTEMPT_LOGIN })
+
+
+        //alert(JSON.stringify(userName));
+    };
+
+
+    //console.log(watch("userName"));
+    //console.log(watch("password"))
+
+    //Add more validations for login and make login screen better looking
     return (
         <div className="Login">
             <h2>Sign In</h2>
             <p>Tip: Login with username: user10, password: Lexicon1&</p>
-            <Form className="form">
-                {loginFailure && (
-                    <Alert color="danger"> Failed to login</Alert>
-                )}
-                <FormGroup>
-                    <Label for="Name">Username</Label>
-                    <Input
-                        type="text"
-                        name="email"
-                        id="Name"
-                        placeholder="oskar11"
-                        value={userName}
-                        onChange={e => setUserName(e.target.value)}
-                    />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="examplePassword">Password</Label>
-                    <Input
-                      type="password"
-                      name="password"
-                      id="examplePassword"
-                      placeholder="********"
-                      value={password}
-                      onChange={e => setPassword(e.target.value)}
-                    />
-                </FormGroup>
-                <Button onClick={handleSubmit} >Submit</Button>
+            
+            <Form onSubmit={handleSubmit(onSubmit)}>
+                <input type="text" placeholder="Username" {...register("userName", { required: true, maxLength: 20 })} />
+                {errors.userName && <span>Username is required</span>}
+                <br />
+                <input type="password" placeholder="********" {...register("password", { required: true, maxLength: 30 })} />
+                {errors.password && <span>Password is required</span>}
+                <br />
+                <input type="submit" value="Login" disabled={!isDirty || !isValid}  />
+                
             </Form>
+
             <Link to="/register">Register new Account</Link>
-      </div>
-  );
+        </div>
+    );
 }
